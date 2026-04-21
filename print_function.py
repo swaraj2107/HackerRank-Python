54fa10cc7005b16ff91b6a7e2ee948c108a05bc7
swaraj kumar <107300954+swaraj2107@users.noreply.github.com>
2026-04-21 06:46:46 +0530

    Create print_function.py

---
+++ print_function.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    temp=1
+    for i in range(n):
+        temp=temp*10
+    count=1
+    for i in range(n):
+        print(count, end="")
+        count=count+1
+        
