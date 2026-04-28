c8efefd6de19009780e995fb725118aa87699994
swaraj kumar <107300954+swaraj2107@users.noreply.github.com>
2026-04-28 11:15:14 +0530

    Create sWAP_cASE.py

---
+++ sWAP_cASE.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    result=""
+    for i in s:
+        if (i.islower()):
+            result = result + i.upper()
+        else:
+            result = result + i.lower()
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
